ecd734d839dbcc1d5612b0e6c3ae54b5037f4ce4
awais786327 <awais3227443@gmail.com>
2019-04-14 03:02:47 +0500

    moved passport stuff

--- src/runner.js
+++ src/runner.js
@@ -9,55 +9,6 @@ let main = function() {
 
 	server.logger.info('FHIR Server successfully validated.');
 
-	// Validating auth using passport
-	const params = {
-		scope: 'openid',
-		respose_types: ['code'],
-		client_id: 'ABCD_CLIENT_ID',
-		client_secret: 'ABCD_CLIENT_SECRETE',
-		redirect_uris: ['https://angular-test-fhir.netlify.com/assets/signin-callback.html'],
-	};
-	// optional, defaults to false, when true req is passed as first argument to verify fn
-	const passReqToCallback = false;
-
-	const usePKCE = true;
-
-	const issuer = new Issuer({
-		issuer: 'https://fhirapp.herokuapp.com',
-		authorization_endpoint: 'https://fhirapp.herokuapp.com/auth',
-		token_endpoint: 'https://fhirapp.herokuapp.com/token',
-		userinfo_endpoint: 'https://fhirapp.herokuapp.com/userinfo',
-		jwks_uri: 'https://fhirapp.herokuapp.com/jwks',
-		code_challenge_methods_supported: ['plain', 'S256']
-	});
-
-	const client = new issuer.Client({
-		client_id: 'ABCD_CLIENT_ID',
-		client_secret: 'ABCD_CLIENT_SECRETE',
-		respose_types: ['code'],
-		redirect_uris: ['https://angular-test-fhir.netlify.com/assets/signin-callback.html'],
-	});
-
-	passport.use('oidc', new Strategy({ client, params, passReqToCallback, usePKCE }, (tokenset, userinfo, done) => {
-		console.log('tokenset', tokenset);
-		console.log('access_token', tokenset.access_token);
-		console.log('id_token', tokenset.id_token);
-		console.log('claims', tokenset.claims);
-		console.log('userinfo', userinfo);
-
-		User.findOne({ id: tokenset.claims.sub }, function (err, user) {
-			if (err) return done(err);
-			return done(null, user);
-		});
-	}));
-
-	server.app.get('/auth', passport.authenticate('oidc'));
-
-	//server.app.get('/auth', function(req, res){
-	//	console.info('/auth');
-	//	res.send('/auth route');
-	//});
-
 	// Start our server
 	server.listen(3000);
 	server.logger.info('FHIR Server listening on localhost:' + 3000);
--- src/strategies/bearer.strategy.js
+++ src/strategies/bearer.strategy.js
@@ -2,39 +2,41 @@ const Strategy = require('passport-http-bearer').Strategy;
 const request = require('superagent');
 const env = require('var');
 
-/**
- * Bearer Strategy
- *
- * This strategy will handle requests with BearerTokens.  This is only a template and should be configured to
- * your AuthZ server specifications.
- *
- * Requires ENV variables for introspecting the token
- */
-module.exports.strategy = new Strategy(
-    function(token, done) {
-		if (!env.INTROSPECTION_URL) {
-			return done(new Error('Invalid introspection endpoint.'));
-		}
+const params = {
+	scope: 'openid',
+	respose_types: ['code'],
+	client_id: 'ABCD_CLIENT_ID',
+	client_secret: 'ABCD_CLIENT_SECRETE',
+	redirect_uris: ['https://angular-test-fhir.netlify.com/assets/signin-callback.html'],
+};
+const passReqToCallback = false;
+const usePKCE = true;
 
-		request
-		.post(env.INTROSPECTION_URL)
-		.set('content-type', 'application/x-www-form-urlencoded')
-		.send({token: token, client_id: env.CLIENT_ID, client_secret: env.CLIENT_SECRET})
-		.then((introspectionResponse) => {
-			const decoded_token = introspectionResponse.body;
+const issuer = new Issuer({
+	issuer: 'https://fhirapp.herokuapp.com',
+	authorization_endpoint: 'https://fhirapp.herokuapp.com/auth',
+	token_endpoint: 'https://fhirapp.herokuapp.com/token',
+	userinfo_endpoint: 'https://fhirapp.herokuapp.com/userinfo',
+	jwks_uri: 'https://fhirapp.herokuapp.com/jwks',
+	code_challenge_methods_supported: ['plain', 'S256']
+});
 
-			if (decoded_token.active) {
-				// TODO: context could come in many forms, you need to decide how to handle it.
-				// it could also be decodedToken.patient etc...
-				let { scope, context, sub, user_id } = decoded_token;
-				let user = {user_id, sub};
+const client = new issuer.Client({
+	client_id: 'ABCD_CLIENT_ID',
+	client_secret: 'ABCD_CLIENT_SECRETE',
+	response_types: ['code'],
+	redirect_uris: ['https://angular-test-fhir.netlify.com/assets/signin-callback.html'],
+});
 
-				// return scopes and context.  Both required
-				return done(null, user, {scope, context});
-			}
+module.exports.strategy = new Strategy({ client, params, passReqToCallback, usePKCE }, (tokenset, userinfo, done) => {
+	console.log('tokenset', tokenset);
+	console.log('access_token', tokenset.access_token);
+	console.log('id_token', tokenset.id_token);
+	console.log('claims', tokenset.claims);
+	console.log('userinfo', userinfo);
 
-			// default return unauthorized
-			return done(new Error('Invalid token'));
-		});
-	}
-);
+	User.findOne({ id: tokenset.claims.sub }, function (err, user) {
+		if (err) return done(err);
+		return done(null, user);
+	});
+});
